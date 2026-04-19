bf7834d9e561a77f14837f28a9fac82101a04205
Bhoomi Aggarwal <bhoomiaggarwal1708@gmail.com>
2026-04-19 17:45:22 +0530

    Help_Alice_to_find_the_Parity.cpp

---
+++ Help_Alice_to_find_the_Parity.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+	long long L, R; 
+	cin>>L>>R;
+	int X=0;
+	for(int i=L; i<=R; i++){
+		X= X^i;
+	}
+	if(X%2==0)
+	cout<<"even";
+	else
+	cout<<"odd";
+
+	return 0;
+
+}
